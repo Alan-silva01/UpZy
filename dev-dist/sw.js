62e2f0c02f77367400bcd1cb1462e3f3a94410a9
Alan Ferreira da Silva <alan.ferreira1123@gmail.com>
2025-12-09 15:41:42 -0300

    -m ajuste desempenho

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.4b474p5dg5"
+    "revision": "0.vl6fpd9meoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
