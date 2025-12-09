796ab7fbf2bb90608aa4b210b71167a223ac4462
Alan Ferreira da Silva <alan.ferreira1123@gmail.com>
2025-12-09 15:27:01 -0300

    -m ajuste logo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.o8jbf8d3614"
+    "revision": "0.89s5s8gsu1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
